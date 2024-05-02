ee9245c5506bda7c132c4a85f1d5691f165fc660
Carlos Peñaloza <carlos2172336@correo.uis.edu.co>
2024-05-02 01:05:01 -0500

    01/05/2024: yml files added

---
+++ reviews-script.js
@@ -0,0 +1,174 @@
+db('test').collection('reviews').insertOne(
+{
+    usuario: "mannulus",
+    isbn: "9789584295446",
+    estrellas: 2,
+    comentario: "no es muy bueno, muy aburrido, perfiero una pelicula"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "chaphe",
+    isbn: "9789584276971",
+    estrellas: 5,
+    comentario: "una excelente guia del autocuidado"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "mapache",
+    isbn: "9789584295446",
+    estrellas: 5,
+    comentario: "Aunque no murio por snuznu muy interesante un poco ficticio"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "javier",
+    isbn: "9789585190603",
+    estrellas: 5,
+    comentario: "este libro me ayudo a comprender el metabolismo de mi cuerpo y a lllevar un estilo de vida más saludable"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "manuel",
+    isbn: "9789585190603",
+    estrellas: 3,
+    comentario: "al menos pase un rato leyendo no me aporto nada pero es facil de leer"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "maria",
+    isbn: "9789585190603",
+    estrellas: 5,
+    comentario: "me calme al leer esto, muybuen libro"
+});
+
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "dayana",
+    isbn: "9789585190300",
+    estrellas: 5,
+    comentario: "Excelente libro lo super recomiendo muy motivador"
+}
+);
+
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "samuel",
+    isbn: "9789585190300",
+    estrellas: 2,
+    comentario: "No proporciona nada de motivacion."
+});
+
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "carlos",
+    isbn: "9789585191066",
+    estrellas: 4,
+    comentario: "por momentos se torna aburrida."
+});
+
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "sammy",
+    isbn: "9789585190366",
+    estrellas: 5,
+    comentario: "uno de los mejores libros que he leido"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "jhon",
+    isbn: "9789585190001",
+    estrellas: 5,
+    comentario: "muy bonita historia para contarles a tus hijos"
+});
+
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "gabriel",
+    isbn: "9789585190001",
+    estrellas: 3,
+    comentario: "muy infantil."
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "GabrielVega",
+    isbn: "10010090321",
+    estrellas: 4,
+    comentario: "Gran libro para aprender a programar, sin embargo me quedó la duda de ¿cuál es la entrada y cuál es la salida?"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "FelipeCabeza",
+    isbn: "10010090321",
+    estrellas: 5,
+    comentario: "Obra magnifica, aprendí a hacer el factorial"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "MarkSuckerberg",
+    isbn: "10010090321",
+    estrellas: 5,
+    comentario: "Me sirvió para hacer Facebook👍🏻😎📘"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "GabrielVega",
+    isbn: "123132131321",
+    estrellas: 5,
+    comentario: "Un clásico de la literatura"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "FelipeCabeza",
+    isbn: "03213128888",
+    estrellas: 5,
+    comentario: "Este libro me cambió la vida... sin palabras"
+}
+);
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "Mohammed",
+    isbn: "0321299999",
+    estrellas: 3,
+    comentario: "No sé qué es docker jaja "
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "GabrielVega",
+    isbn: "0321299999",
+    estrellas: 4,
+    comentario: "Me sirvió para el trabajo de software :)"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "FelipeCabeza",
+    isbn: "10010090321",
+    estrellas: 4,
+    comentario: "Estaba tan bueno que me lo compré otra vez, pero esta vez no me gustó tanto 😕"
+});
+
+db('test').collection('reviews').insertOne(
+{
+    usuario: "MarkSuckerberg",
+    isbn: "9789584276971",
+    estrellas: 4,
+    comentario: "Buen libro"
+});
